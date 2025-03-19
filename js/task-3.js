30a95aba7f69e0db4c9b7ad969e9efc57efcd743
Olya <obsmyk202001@gmail.com>
2025-03-19 14:29:00 +0100

    add all tasks

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,15 @@
-function getElementWidth(content, padding, border) {
-  return Number.parseFloat(content) + Number.parseFloat(padding) * 2 + Number.parseFloat(border) * 2;
+function filterArray(numbers, value) {
+  const numbersArrey = [];
+  for (const number of numbers) {
+    if (number > value) {
+      numbersArrey.push(number);
+    }
+  }
+  return numbersArrey;
 }
 
-
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
